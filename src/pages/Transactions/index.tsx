fba572f9fd96cbbdabb618f75691953204487172
Augusto Gomes <augusto.tgomes@gmail.com>
2023-03-15 18:06:27 -0300

    criado componente de busca, tabela de transações e Sumario

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,41 @@
 import { Header } from '../../components/Header'
+import { SearchForm } from '../../components/SearchForm';
+import { Summary } from '../../components/Summary';
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from './styles';
 
 export function Transactions() {
   return(
     <div>
       <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento do site</td>
+              <td>
+                <PriceHighLight variant='income'>
+                  R$ 12.000,00
+                </PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Contas</td>
+              <td>
+                <PriceHighLight variant='outcome'>
+                  -R$ 5.000,00
+                </PriceHighLight> 
+              </td>
+              <td>contas</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
