18965ad0481b8f140a5ba6c9d68c4ac68b03efbc
Augusto Gomes <augusto.tgomes@gmail.com>
2023-03-21 18:27:04 -0300

    terminado estilização dos radios button entrada e saída do modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import * as Dialog from '@radix-ui/react-dialog'
+ 
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
 import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles';
 
@@ -19,18 +20,18 @@ export function NewTransactionModal(){
         <input type="text" placeholder='Categoria' required />
 
         <TransactionType>
-          <TransactionTypeButton variant='income'>
+          <TransactionTypeButton variant='income' value='income'>
             <ArrowCircleUp size={24} />
             Entrada
           </TransactionTypeButton>
 
-          <TransactionTypeButton variant='outcome'>
+          <TransactionTypeButton variant='outcome' value='outcome'>
             <ArrowCircleDown size={24} />
             Saída
           </TransactionTypeButton>
         </TransactionType>
 
-        <button type='submit'> 
+        <button type='submit'>
           Cadastrar
         </button>
       </form>
